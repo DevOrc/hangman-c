a2624fadc4cb4e199a5baa594efacb55620b8845
DevOrc <ncharlton002@gmail.com>
2018-01-21 15:09:36 -0500

    Generate word from file

--- words.c
+++ words.c
@@ -1,30 +1,41 @@
 #include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int WORD_LENGTH = 15;
+int NUM_OF_WORDS = 640;
 
 // Get a random answer to guess
 char *getAnswer(){
+	int c;
+	char lines[NUM_OF_WORDS][WORD_LENGTH];
+	char line[WORD_LENGTH];
+	int count;
+	FILE *file;
+	file = fopen("words.txt", "r");
+
+	if (file) {
+        while(fgets(line, WORD_LENGTH, file) != NULL){
+			for(int i=0; i<WORD_LENGTH;i++){
+				if(line[i] == '\n')
+					line[i] = '\0';
+			}
+
+			strcpy(lines[count], line);
+			count++;
+    	}
+
+    	fclose(file);
+	}else{
+		printf("\nProblem opening words.txt. \n");
+		exit(1);
+	}
+
 	srand(time(NULL));
 	int r = rand() % 10;
+	char *output = malloc(WORD_LENGTH);  
+	strcpy(output, lines[r]);
 
-	switch (r){
-	case 0:
-		return "TIGER";
-	case 1:
-		return "COMPUTER";
-	case 2:
-		return "LAMP";
-	case 3:
-		return "HOUSE";
-	case 4:
-		return "FOOTBALL";
-	case 5:
-		return "SODA";
-	case 6:
-		return "FOLDER";
-	case 7: 
-		return "DOOR";
-	case 8:
-		return "ROAD";
-	case 9: 
-		return "TALL";
-	}
+	return output;
 }
