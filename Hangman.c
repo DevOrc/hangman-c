b905229207f5bf3fd0e88d10dd2e956fa6d27e17
DevOrc <ncharlton002@gmail.com>
2018-02-05 09:14:58 -0500

    Move Util functions to seperate file

--- Hangman.c
+++ Hangman.c
@@ -5,6 +5,7 @@
 #include "Hangman.h"
 #include "words.c"
 #include "settings.c"
+#include "util.c"
 
 int main(){
 	//Clear the screengit
@@ -132,41 +133,6 @@ int main(){
 	waitForEnter();
 	waitForEnter();
 	system("cls");
-}
-
-//Checks if a character is contained within a string.
-//Returns 1 if the character is found and 0 if it isn't.
-int strcont(char *str, char c){
-	for (int i = 0; i < strlen(str); i++){
-		if (str[i] == c)
-			return 1;
-	}
-	return 0;
-}
-
-//Waits for enter to be pressed by the user
-void waitForEnter(){
-	char c;
-
-	while (1){
-		scanf("%c", &c);
-
-		if(c == '\n'){
-			return;
-		}
-	}
-}
-
-// Check if debug mode has been enabled
-void checkDebug(){
-	const char *debugEnv = getenv("DEBUG");
-
-	if(debugEnv != NULL){
-		if (strcmp(debugEnv, "1") == 0){
-			DEBUG = 1;
-			printf("DEBUG Mode Enabled!\n");
-		}
-	}
 }
 
 // Get the character guessed by the player
